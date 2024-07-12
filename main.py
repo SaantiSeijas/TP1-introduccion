6485550751bf421cb3aa3b9678637df397eb4cf7
santi <santi@DESKTOP-NI9120R>
2024-07-12 12:49:44 -0300

    Corrijo nombre de funcion hello_world

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ app=Flask(__name__)
 port=5000
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 @app.route('/')
-def hello world():
+def hello_world():
     return 'Hola'
 
 if __name__=='__main__':
     print("Starting server")
-    
